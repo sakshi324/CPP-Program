8a3f69ec77613d6282bfe0ca07b447f6b983ac86
Sakshi patil <84581028+sakshi324@users.noreply.github.com>
2022-08-09 13:54:01 +0530

    Delete cc.c

--- cc.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>    
-int main(){    
-int number=0;    
-printf("enter a number:");    
-//scanf("%d",&number);    
-printf("cube of number is:%d ",number*number*number);    
-return 0;  
-}    
